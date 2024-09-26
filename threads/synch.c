e76718d1ab337eb2f5e8c9151e5a79ff63b0ec77
changmo <kmaestro043@gmail.com>
2024-09-26 15:41:52 +0000

    P2/All passed w.Outcomes

--- threads/synch.c
+++ threads/synch.c
@@ -119,7 +119,7 @@ void sema_up (struct semaphore *sema) {
 		thread_unblock (list_entry (list_pop_front (&sema->waiters),struct thread, elem));
 	}
 	sema->value++;
-    preempt();
+   preempt();
 	intr_set_level (old_level);
 }
 
